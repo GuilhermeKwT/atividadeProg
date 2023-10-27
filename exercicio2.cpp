f532ffc4a218d9344e58f1ae299e8d9d3fff48f4
Guilherme Kwaczynski <139267201+GuilhermeKwT@users.noreply.github.com>
2023-10-26 22:17:29 -0300

    Update exercicio2.cpp

--- exercicio2.cpp
+++ exercicio2.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(){
     int v[4] = {1, 8, 9, 9}, *pV;
+    pV = &v[0];
     
     for (int i = 0; i < 4; i++){
-        pV = &v[i];
-        cout << *pV << ' ';
+        cout << *(pV + i) << ' ';
     }
 }
