cd7d9234732b1fc9fc4289b4650a5b5fcdac55a2
Guilherme Kwaczynski <139267201+GuilhermeKwT@users.noreply.github.com>
2024-01-30 21:54:02 -0300

    Add files via upload

---
+++ pag11_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int func(int n){
+    int s = 0;
+    for(int i = 0; i <= n; i++){
+        s += i;
+    }
+    return s;
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+    cout << "1 " << func(n) << endl;
+
+    return 0;
+}
